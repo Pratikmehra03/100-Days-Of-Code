5d5a65990b3f635559e13c4f4ed0f690b3f26802
Pratik Mehra <pratikmehra66@gmail.com>
2025-12-03 17:08:50 +0000

    Day 76 completed

---
+++ DAY076/q126.c
@@ -0,0 +1,28 @@
+/* Ask the user for a filename. 
+   Check if it exists by trying to open it in read mode. 
+   If the file pointer is NULL, print an error message; 
+   otherwise, read and display its content.
+*/
+#include <stdio.h>
+
+int main() {
+    char fname[100];
+    FILE *fp;
+    char line[300];
+
+    printf("Enter filename: ");
+    scanf(" %s", fname);
+
+    fp = fopen(fname, "r");
+
+    if(fp == NULL) {
+        printf("File does not exist.\n");
+        return 0;
+    }
+
+    while(fgets(line, sizeof(line), fp) != NULL)
+        printf("%s", line);
+
+    fclose(fp);
+    return 0;
+}
