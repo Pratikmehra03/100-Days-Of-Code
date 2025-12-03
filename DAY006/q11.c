af68c7b8ad3676a7b38624a8580a6d73706aaf43
Pratik Mehra <pratikmehra66@gmail.com>
2025-12-03 08:03:17 +0000

    Day 6 Completed

---
+++ DAY006/q11.c
@@ -0,0 +1,15 @@
+/* Write a program to check whether a number is even or odd. */
+#include <stdio.h>
+
+int main() {
+    int n;
+    printf("Enter a number: ");
+    scanf("%d", &n);
+
+    if(n % 2 == 0)
+        printf("Even\n");
+    else
+        printf("Odd\n");
+
+    return 0;
+}
